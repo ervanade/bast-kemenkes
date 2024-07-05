b8a9da4d44f118f19ebe8c3a4d75ca2182392e6c
Ervan Ade Wijaya <89782667+ervanade@users.noreply.github.com>
2024-07-05 22:11:53 +0700

    fix

--- src/components/Header/DropdownUser.jsx
+++ src/components/Header/DropdownUser.jsx
@@ -77,7 +77,7 @@ const DropdownUser = () => {
                 My Profile
               </Link>
             </li>
-            <li>
+            {/* <li>
               <Link
                 to="#"
                 className="flex items-center gap-3.5 text-sm font-medium duration-300 ease-in-out hover:text-primary lg:text-base"
@@ -97,7 +97,7 @@ const DropdownUser = () => {
                 </svg>
                 My Contacts
               </Link>
-            </li>
+            </li> */}
             <li>
               <Link
                 to="/settings"
